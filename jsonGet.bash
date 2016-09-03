bb8d390eb82f7c25c89c0a8a2eea1e4aedad9f53
brokendish <brokendish@gmail.com>
2016-09-03 18:45:22 +0900

    mod

--- jsonGet.bash
+++ jsonGet.bash
@@ -14,39 +14,42 @@ echo "targetFile:$targetFile"
 ret=`echo "$jsondat"|grep $targetFile`
 jsondat=$ret
 
-
-echo "----- set enviroment ---------------"
-echo "$jsondat"|while read line
-do
-	bkupenv=
-	echo "[Line data]:--------------------:"$line
-	for column in $line;
+if [ -n "$jsondat" ]; then
+	echo "----- set enviroment ---------------"
+	echo "$jsondat"|while read line
 	do
-		#echo "----- get enviroment ---------------"
-		env=`echo $column|cut -d"=" -f1`
-		echo "ENVIRON-----:$env"
-
-		#echo "----- backup enviroment ---------------"
-		aa=`eval echo "$"$env`
-		bkupenv="$bkupenv $env=$aa"
-
-		#echo "----- unset  enviroment ---------------"
-		eval "export $env="
-
-		echo "----- export enviroment ---------------"
-		echo "[Column data]:-----:$column"
-		eval "export $column"
+		bkupenv=
+		echo "[Line data]:--------------------:"$line
+		for column in $line;
+		do
+			#echo "----- get enviroment ---------------"
+			env=`echo $column|cut -d"=" -f1`
+			echo "ENVIRON-----:$env"
+	
+			#echo "----- backup enviroment ---------------"
+			aa=`eval echo "$"$env`
+			bkupenv="$bkupenv $env=$aa"
+	
+			#echo "----- unset  enviroment ---------------"
+			eval "export $env="
+	
+			echo "----- export enviroment ---------------"
+			echo "[Column data]:-----:$column"
+			eval "export $column"
+		done
+	
+		echo "----- execute Generate Process ---------------"
+	
+		echo "----- restore enviroment ---------------"
+		echo "bkupenv-----$bkupenv"
+		for res in $bkupenv;
+		do
+			eval "export $res"
+		done
 	done
-
+else
 	echo "----- execute Generate Process ---------------"
-
-	echo "----- restore enviroment ---------------"
-	echo "bkupenv-----$bkupenv"
-	for res in $bkupenv;
-	do
-		eval "export $res"
-	done
-done
-
+	
+fi
 
 
